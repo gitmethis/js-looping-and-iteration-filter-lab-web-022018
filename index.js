319466ffb08c4aad4ec700fc0d83a48efbbd71df
Chase <chaseit12@gmail.com>
2018-04-01 20:07:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Code your solution in this file
 
 
+// Code your solution in this file
+
+
 function findMatching(the_arr, thing_to_be_looked_for){
   return the_arr.filter(function(each_item){
     return each_item.toLowerCase() == thing_to_be_looked_for.toLowerCase()
@@ -10,7 +13,9 @@ function findMatching(the_arr, thing_to_be_looked_for){
 function fuzzyMatch(the_arr, the_beginning_part){
   return the_arr.filter(function(each_item){
     // NOW LOOP THROUGH EACH CHARACTER TO CHECK IF ITS THE SAME AND IN ORDER
-    for (let character of the_arr)
+    for (let i = 0;i < the_arr.length; i++){
+      
+    }
   })
 }
 
