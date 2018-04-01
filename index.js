449479b0df56d583f92ae551264b1f528fdb460c
Chase <chaseit12@gmail.com>
2018-04-01 20:12:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,19 +11,10 @@ function findMatching(the_arr, thing_to_be_looked_for){
 }
 
 function fuzzyMatch(the_arr, the_beginning_part){
-  return the_arr.filter(function(each_item){
-    // NOW LOOP THROUGH EACH CHARACTER TO CHECK IF ITS THE SAME AND IN ORDER
-     new_arr = []
-     for (let word of the_arr){
-        for (let i = 0;i < the_beginning_part.length; i++){
-             if (word[i] === the_beginning_part[i]){
-                continue;
-             }else{
-                 break;
-             }
-        }
-     }
-  })
+ let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
 }
 
 function matchName(){
