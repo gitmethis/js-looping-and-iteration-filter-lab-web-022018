c40210628c4d94989931d482cfdf650bad4cdb1c
Chase <chaseit12@gmail.com>
2018-04-01 20:10:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ function findMatching(the_arr, thing_to_be_looked_for){
 function fuzzyMatch(the_arr, the_beginning_part){
   return the_arr.filter(function(each_item){
     // NOW LOOP THROUGH EACH CHARACTER TO CHECK IF ITS THE SAME AND IN ORDER
+     new_arr = []
      for (let word of the_arr){
-       
+        for (let i = 0;i < the_beginning_part.length; i++){
+             if (word[i] == )
+        }
      }
   })
 }
